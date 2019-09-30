1e258627992fd23cea2525d21b45c569fc808d3b
Sandra Vilder <sandra.vilder@gmail.com>
2019-09-30 17:03:05 +0200

    bug js

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import '../stylesheets/application.scss';
-import { initUpdateNavbarOnScroll } from 'navbar';
+import { initUpdateNavbarOnScroll } from 'components/navbar';
 initUpdateNavbarOnScroll();
