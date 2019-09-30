39d34cd691a9f150ccf6e4cf6ffa7b2b4ca6912b
Sandra Vilder <sandra.vilder@gmail.com>
2019-09-30 18:05:16 +0200

    fix

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,16 +1,3 @@
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import '../stylesheets/application.scss';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-=======
-import { initUpdateNavbarOnScroll } from 'navbar';
->>>>>>> master
-=======
-import { initUpdateNavbarOnScroll } from 'components/navbar';
->>>>>>> master
-initUpdateNavbarOnScroll();
-=======
->>>>>>> master
